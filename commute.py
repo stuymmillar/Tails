1c42ee90cbc0ce2f0cdbd93132a6043e97c2091f
Max Millar <mmillar@stuy.edu>
2018-11-29 12:36:32 -0500

    added more to commute.py

--- commute.py
+++ commute.py
@@ -10,13 +10,13 @@ def render_test():
     #print(schooldata[0])
     homelat = 40.775088
     homelong = -73.977815
-    #schoollat = 40.71759
-    #schoollong = -74.013748
-    schoollat = schooldata[0]["latitude"]
-    schoollong = schooldata[0]["longitude"]
+    schoollat = 40.71759
+    schoollong = -74.013748
+    #schoollat = schooldata[0]["latitude"]
+    #schoollong = schooldata[0]["longitude"]
     url = "https://transit.api.here.com/v3/route.json?app_id=7jNm9uNlO3Up0edHuqK0&app_code=6QxzobcNH0yyWBd27YItEg&routing=all&dep=" + str(homelat) + "," + str(homelong) + "&arr=" + str(schoollat) + "," + str(schoollong) + "&time=2018-11-27T07%3A30%3A00"
     data = json.loads((request.urlopen(url)).read())
-    return str(data)
+    return str(data["Res"]["Connections"]["Connection"][1]["Sections"]["Sec"][0]["Dep"]["Stn"]["name"]) + " to " + str(data["Res"]["Connections"]["Connection"][1]["Sections"]["Sec"][len(data["Res"]["Connections"]["Connection"][1]["Sections"]["Sec"]) - 1]["Dep"]["Stn"]["name"])
 
 if __name__ == '__main__':
     app.debug = True
