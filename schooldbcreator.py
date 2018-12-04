3571be9b348b005644068d0808ae60c05031fe2f
ijon9 <ijon@stuy.edu>
2018-12-04 12:29:12 -0500

    Did some school db stuff

--- schooldbcreator.py
+++ schooldbcreator.py
@@ -14,10 +14,10 @@ url = "https://data.cityofnewyork.us/resource/g2qs-86ey.json"
 schoollist = json.loads(urllib.request.urlopen(url).read())
 
 def generateTable():
-    c.execute("CREATE TABLE schools(schoolNumber INTEGER, schoolname TEXT, latitude DECIMAL, longitude DECIMAL)")
+    c.execute("CREATE TABLE schools(schoolNumber INTEGER, schoolname TEXT, latitude DECIMAL, longitude DECIMAL)")# , statement TEXT)")
     for school in schoollist:
         entryNum = 0
-        command = 'INSERT INTO schools VALUES({0},"{1}",{2},{3})'.format(entryNum, school['school_name'], school['latitude'], school['longitude'])
+        command = 'INSERT INTO schools VALUES({0},"{1}",{2},{3})'.format(entryNum, school['school_name'], school['latitude'], school['longitude'])#, school['overview_paragraph'])
         c.execute(command)
         entryNum += 1
 
