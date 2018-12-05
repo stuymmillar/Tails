adb184d0480ba5a2528f996f6f9529f5196ea803
Brian <hlee5@stuy.edu>
2018-12-05 12:15:54 -0500

    Change init

--- schooldbcreator.py
+++ schooldbcreator.py
@@ -15,11 +15,10 @@ schoollist = json.loads(urllib.request.urlopen(url).read())
 
 def generateTable():
     c.execute("CREATE TABLE schools(schoolNumber INTEGER, schoolname TEXT, latitude DECIMAL, longitude DECIMAL, statement TEXT)")
-    entryNum = 0
     for school in schoollist:
         #command = 'INSERT INTO schools VALUES({0},"{1}",{2},{3},"{4}")'.format(entryNum, school['school_name'], school['latitude'], school['longitude'], school['overview_paragraph'])
         command = 'INSERT INTO schools VALUES(?,?,?,?,?)'
-        c.execute(command, (entryNum, school['school_name'], school['latitude'], school['longitude'], school['overview_paragraph']))
+        c.execute(command, (274, school['school_name'], school['latitude'], school['longitude'], school['overview_paragraph']))
         entryNum += 1
 
 generateTable()
