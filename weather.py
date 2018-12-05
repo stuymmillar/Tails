2cdce17fd642f971bfb856556b6a0b39ee049561
unknown <ijon@stuy.edu>
2018-12-05 06:26:29 -0500

    Added weather api key to keys.json

--- weather.py
+++ weather.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def helloWorld():
-    #Weather api
-    #api_key = "49c480c99387a9288020a78b60fb253e"
-    url = "https://api.openweathermap.org/data/2.5/weather?zip=10282,us&appid=49c480c99387a9288020a78b60fb253e"
-    #url = url.replace('[]', api_key)
+    url = "https://api.openweathermap.org/data/2.5/weather?zip=10282,us&appid="
+    with open('keys.json') as f:
+        key = json.load(f)["weather"]
+    url += key
     result = json.loads(urllib.request.urlopen(url).read())
     return render_template('weather.html', u=result)
 
